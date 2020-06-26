0d93817bb679fe703034ce1b9901e5bbb291f693
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-06-26 19:19:57 -0400

    write component with class and fill it with state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 2
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h2>Count : {this.state.count}</h2>
+        <button>Deccrement</button>
+        <button>Increment</button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
