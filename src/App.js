0f5521fb6252bdf2eb0a352fd2587b9d96a4411c
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-06-26 19:26:27 -0400

    main

--- src/App.js
+++ src/App.js
@@ -5,16 +5,26 @@ import './App.css'
 class App extends Component {
   constructor(props) {
     super(props)
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
     this.state = {
       count: 2
     }
+
+  }
+
+  increment() {
+    console.log('Increment');
+  }
+  decrement() {
+    console.log('Decrement');
   }
   render() {
     return (
       <div>
         <h2>Count : {this.state.count}</h2>
-        <button>Deccrement</button>
-        <button>Increment</button>
+        <button onClick={this.decrement}>Deccrement</button>
+        <button onClick={this.increment}>Increment</button>
       </div>
     )
   }
